3089dc6ff5e6097d60f9e36cf389ca14aa039d7a
Chris Zhang <zcyjim@outlook.com>
2022-09-01 14:25:36 +0800

    Added IfLetElse and IfElse to support else in for_ch!

--- for_ch_test/src/main.rs
+++ for_ch_test/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         for x in 0..10;
         for y in 0..x, for _ in 0..5;
         if let Some(z) = Some(2);
-        if x - y < z;
-        println!("{:?}", (x, y));
+        if x - y < z else { println!("x - y >= 2: {:?}", (x, y)); } ;
+        println!("x - y <  2: {:?}", (x, y));
     }
 }
